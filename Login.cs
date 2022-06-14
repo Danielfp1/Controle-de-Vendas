f61aa00ecbfbd534895e5df1c0cc0b09aa65ccc7
Daniel Figueiredo Pereira <danielfp.pc@hotmail.com>
2022-06-14 20:32:08 -0300

    Adicionado Login

--- Login.cs
+++ Login.cs
@@ -6,15 +6,17 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using System.Windows.Forms;
 
 
 namespace Controle_de_Vendas
 {
     public partial class Login : Form
     {
-        
+      
         public Login()
         {
             InitializeComponent();
@@ -25,9 +27,27 @@ namespace Controle_de_Vendas
 
         }
 
-        private void button_login_Click(object sender, EventArgs e)
+        private async void button_login_Click(object sender, EventArgs e)
         {
-            
+            //Adicionar if de conecção com a internet
+            FirebaseConfig config = new FirebaseConfig
+            {
+                AuthSecret = FirebaseConfigConnection.AuthScrete,
+                BasePath = FirebaseConfigConnection.BasePath
+            };
+            IFirebaseClient client = new FireSharp.FirebaseClient(config);
+
+            FirebaseResponse firebaseResponse = await client.GetTaskAsync("Users/" + textBox_email.Text);
+            CadastroData cadastroData = firebaseResponse.ResultAs<CadastroData>();
+
+            if (EncryptSHA.GetShaData(textBox_Senha.Text) == cadastroData.Password)
+            {
+                MessageBox.Show("Logado!");
+            }
+            else
+            {
+                MessageBox.Show("Falha no Login!");
+            }
         }
 
         private void button_Cadastar_Click(object sender, EventArgs e)
