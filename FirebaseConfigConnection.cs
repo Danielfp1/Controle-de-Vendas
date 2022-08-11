d91b8eb172dd960b4feb58328629ef5a44d514d9
Daniel Figueiredo Pereira <danielfp.pc@hotmail.com>
2022-08-11 17:45:53 -0300

    Add ConfigurationManager for FirebaseConfigConnection

--- FirebaseConfigConnection.cs
+++ FirebaseConfigConnection.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using System.Configuration;
+
 
 namespace Controle_de_Vendas
 {
     class FirebaseConfigConnection
     {
-        public const string BasePath = "https://controle-de-vendas-87cdc-default-rtdb.firebaseio.com/";
-        public const string AuthScrete = "l7zssI97IQ6LIZNtT3tQ7v3UXDCQ72HYGP5NtZwM";
+        public string BasePath = ConfigurationManager.AppSettings["BasePath"];
+        public string AuthScrete = ConfigurationManager.AppSettings["AuthScrete"];
     }
 }
